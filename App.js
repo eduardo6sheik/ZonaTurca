6808d7d8eee6b4e29131d11a13baf0bbe0111f0e
luis6sheik <eduardo6sheik@gmail.com>
2020-04-18 02:53:53 -0400

    instalacion de notificaciones terminada

--- App.js
+++ App.js
@@ -6,8 +6,28 @@ import {
   View,
   Text,
 } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default class MyWeb extends Component {
+  componentDidMount(){
+    this.activivarNotificaciones();
+    this.permisoParaResivirNotificaiones();
+  }
+
+  activivarNotificaciones = async () =>{
+    const activar = await messaging().registerDeviceForRemoteMessages();
+    console.log(activar);
+  }
+
+  permisoParaResivirNotificaiones = async () =>{
+
+    const permiso = await messaging().requestPermission();
+
+    if (permiso) {
+      console.log('Permission settings:', permiso);
+    }
+  }
+
   render() {
     return (
       <View style={{ backgroundColor: 'blue',flex: 1 }}>
